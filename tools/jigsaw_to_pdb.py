022be1bd2ab2e49e38574cf306b8df75edee7ae3
Andy Thomason <andy@andythomason.com>
2015-05-26 18:26:35 +0100

    add command line arg to tools/jigsaw_to_pdb.py

--- tools/jigsaw_to_pdb.py
+++ tools/jigsaw_to_pdb.py
@@ -1,10 +1,13 @@
+import sys
 
 #ATOM      1  N   ILE E  16      18.871  65.715  12.731  1.00 21.86           N  
 
 atom = 1
-for line in open(r"C:\Users\Andy\Google Drive\BioBlox\JigSawFive.ma"):
+for line in open(sys.argv[1]):
     if ".rp" in line:
         pos = line.split()[4:7]
         print("ATOM  %5d  N   ILE E  16     %7.3f %7.3f %7.3f  1.00 21.86           N " % (atom, float(pos[0]),float(pos[1]), float(pos[2])))
         atom = atom + 1
+
+print("TER     ");
 
