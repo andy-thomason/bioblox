49d88d78ba842149eeb2065057ef17e9feebb14f
Andy Thomason <andy@andythomason.com>
2016-02-29 12:15:42 +0000

    add build system for PC

---
+++ PDB_unity/Assets/Editor/PC_Builder.cs
@@ -0,0 +1,11 @@
+﻿// C# example
+using UnityEditor;
+
+class PC_Builder
+{
+  static void PerformBuild()
+  {
+    string[] scenes = { "Assets/main.unity" };
+    BuildPipeline.BuildPlayer(scenes, "build/bioblox.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+  }
+}
---
+++ build_pc_version.bat
@@ -0,0 +1,4 @@
+
+"C:\Program Files\Unity\Editor\Unity.exe" -batchmode -quiet -projectPath %CD%\PDB_Unity -executeMethod PC_Builder.PerformBuild -quit
+echo Result is in PDB_Unity\build\bioblox.exe
+
