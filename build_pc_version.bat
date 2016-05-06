031d9ba9cef7b2a436eeeb927f1da933e6f79c6c
Andy Thomson <andy@andythomason.com>
2016-05-06 17:48:43 +0100

    change build script

--- build_pc_version.bat
+++ build_pc_version.bat
@@ -1,4 +1,4 @@
 
-"C:\Program Files\Unity\Editor\Unity.exe" -batchmode -quiet -projectPath %CD%\PDB_Unity -executeMethod PC_Builder.PerformBuild -quit
-echo Result is in PDB_Unity\build\bioblox.exe
+"C:\Program Files\Unity\Editor\Unity.exe" -batchmode -quiet -projectPath %CD%\client -executeMethod PC_Builder.PerformBuild -quit
+echo Result is in client\build\bioblox.exe
 
